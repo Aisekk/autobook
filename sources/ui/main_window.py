811dfcf1e661be75d34f51b8dc77b24a2d6fe745
Alexander Chekmarev <gooletters@gmail.com>
2023-08-14 21:22:06 +0300

    Added StructUI

--- sources/ui/main_window.py
+++ sources/ui/main_window.py
@@ -11,74 +11,37 @@ from PySide6.QtWidgets import (
     QStackedWidget,
 )
 from PySide6.QtGui import QAction, QIcon
-
-import ui.control_widget
-import ui.car_addition_dialog
-import ui.project_settings_dialog
-import ui.widgets.engine_widget
-
+import ui.struct_ui
 import data_store.data_store as storage
 
 
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         QMainWindow.__init__(self, parent)
+        self.__ui = ui.struct_ui.StructUi(self)
         self.setWindowTitle(self.tr("Autobook"))
         self.setWindowIcon(QIcon("./sources/icons/cabriolet.png"))
-        self.__createMenus()
-        self.__carAdditionDialog = ui.car_addition_dialog.CarAdditionDialog(self)
-        self.__projectSettingsDialog = ui.project_settings_dialog.ProjectSettingsDialog(
-            self
-        )
-        self.__controlWidget = ui.control_widget.ControlWidget()
-        self.__stackedWidget = QStackedWidget()
-        self.__emptyWidget = QWidget()
-        self.__engineWidget = ui.widgets.engine_widget.EngineWidget()
+        self.__ui.createMenus(self)       
 
-        self.__stackedWidget.addWidget(self.__emptyWidget)
-        self.__stackedWidget.addWidget(self.__engineWidget)
-
-        centralWidget = QWidget(self)
         hbxMainLayout = QHBoxLayout()
-        hbxMainLayout.addWidget(self.__controlWidget, 10)
-        hbxMainLayout.addWidget(self.__stackedWidget, 40)
-        centralWidget.setLayout(hbxMainLayout)
-        self.setCentralWidget(centralWidget)
+        hbxMainLayout.addWidget(self.__ui.controlWidget, 10)
+        hbxMainLayout.addWidget(self.__ui.stackedWidget, 40)
+        self.__ui.centralWidget.setLayout(hbxMainLayout)
+        self.setCentralWidget(self.__ui.centralWidget)
 
-        #self.__stackedWidget.setCurrentWidget(self.__emptyWidget)
-        self.__stackedWidget.setCurrentWidget(self.__engineWidget)
+        #self.__stackedWidget.setCurrentWidget(self.__ui.emptyWidget)
+        self.__ui.stackedWidget.setCurrentWidget(self.__ui.engineWidget)
 
         self.__connectObjects()
         # button = QPushButton("From Go: backend.Add(10,99) = %d" % lib.Add(10,99))
         # self.setCentralWidget(button)
 
-    def __createMenus(self) -> None:
-        carMenu = self.menuBar().addMenu(self.tr("Car"))
-        self.__addCarAction = QAction(self.tr("Add") + "...", self)
-        self.__editCarAction = QAction(self.tr("Edit"), self)
-        self.__removeCarAction = QAction(self.tr("Remove"), self)
-        carMenu.addAction(self.__addCarAction)
-        carMenu.addAction(self.__editCarAction)
-        carMenu.addAction(self.__removeCarAction)
-
-        garageMenu = self.menuBar().addMenu(self.tr("Garage"))
-        self.__openGarageAction = QAction(self.tr("Open"), self)
-        garageMenu.addAction(self.__openGarageAction)
-
-        settingsMenu = self.menuBar().addMenu(self.tr("Settings"))
-        self.__projectAction = QAction(self.tr("Project"), self)
-        settingsMenu.addAction(self.__projectAction)
-
-        infoMenu = self.menuBar().addMenu(self.tr("Info"))
-        self.__aboutProgramAction = QAction(self.tr("About program"), self)
-        infoMenu.addAction(self.__aboutProgramAction)
-
     def __connectObjects(self) -> None:
-        self.__addCarAction.triggered.connect(lambda: self.__carAdditionDialog.show())
-        self.__projectAction.triggered.connect(
-            lambda: self.__projectSettingsDialog.show()
+        self.__ui.addCarAction.triggered.connect(lambda: self.__ui.carAdditionDialog.show())
+        self.__ui.projectAction.triggered.connect(
+            lambda: self.__ui.projectSettingsDialog.show()
         )
-        self.__aboutProgramAction.triggered.connect(
+        self.__ui.aboutProgramAction.triggered.connect(
             lambda: QMessageBox.information(
                 self,
                 self.tr("About program"),
---
+++ sources/ui/struct_ui.py
@@ -0,0 +1,52 @@
+from PySide6.QtWidgets import (
+    QWidget,
+    QPushButton,
+    QStackedWidget,
+)
+from PySide6.QtGui import QAction, QIcon
+
+import ui.control_widget
+import ui.car_addition_dialog
+import ui.project_settings_dialog
+import ui.widgets.engine_widget
+
+
+class StructUi(object):
+    def __init__(self, mainWindow) -> None: 
+        self.carAdditionDialog = ui.car_addition_dialog.CarAdditionDialog(mainWindow)
+        self.projectSettingsDialog = ui.project_settings_dialog.ProjectSettingsDialog(
+            mainWindow
+        )
+        self.controlWidget = ui.control_widget.ControlWidget(mainWindow)
+
+        self.stackedWidget = QStackedWidget()
+        self.emptyWidget = QWidget()
+        self.engineWidget = ui.widgets.engine_widget.EngineWidget()
+
+        self.stackedWidget.addWidget(self.emptyWidget)
+        self.stackedWidget.addWidget(self.engineWidget)
+
+        self.centralWidget = QWidget(mainWindow)
+
+        self.addCarAction = QAction(mainWindow.tr("Add") + "...", mainWindow)
+        self.editCarAction = QAction(mainWindow.tr("Edit"), mainWindow)
+        self.removeCarAction = QAction(mainWindow.tr("Remove"), mainWindow)
+        
+        self.openGarageAction = QAction(mainWindow.tr("Open"), mainWindow)
+        self.projectAction = QAction(mainWindow.tr("Project"), mainWindow)
+        self.aboutProgramAction = QAction(mainWindow.tr("About program"), mainWindow)
+
+    def createMenus(self, mainWindow) -> None:
+        carMenu = mainWindow.menuBar().addMenu(mainWindow.tr("Car"))
+        carMenu.addAction(self.addCarAction)
+        carMenu.addAction(self.editCarAction)
+        carMenu.addAction(self.removeCarAction)
+
+        garageMenu = mainWindow.menuBar().addMenu(mainWindow.tr("Garage"))
+        garageMenu.addAction(self.openGarageAction)
+
+        settingsMenu = mainWindow.menuBar().addMenu(mainWindow.tr("Settings"))
+        settingsMenu.addAction(self.projectAction)
+
+        infoMenu = mainWindow.menuBar().addMenu(mainWindow.tr("Info"))
+        infoMenu.addAction(self.aboutProgramAction)
